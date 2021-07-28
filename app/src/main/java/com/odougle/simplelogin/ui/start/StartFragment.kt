b9d65359101ba2a1265b8eebcf07093e69e8def1
douguizilla <douglasgommez@gmail.com>
2021-07-28 12:33:21 -0300

    set OnButtonClicked

--- app/src/main/java/com/odougle/simplelogin/ui/start/StartFragment.kt
+++ app/src/main/java/com/odougle/simplelogin/ui/start/StartFragment.kt
@@ -1,13 +1,17 @@
 package com.odougle.simplelogin.ui.start
 
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.odougle.simplelogin.R
+import kotlinx.android.synthetic.main.fragment_start.*
 
 class StartFragment : Fragment() {
+
+    private lateinit var listener: OnButtonClicked
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -15,6 +19,21 @@ class StartFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_start, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        startButton.setOnClickListener {
+            listener.buttonClicked()
+        }
+    }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        if(context is OnButtonClicked){
+            listener = context
+        }
     }
 
     companion object {
