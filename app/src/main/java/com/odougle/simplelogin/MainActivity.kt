2bfce60673eb9ec432d71603989a2662daf97ad9
douguizilla <douglasgommez@gmail.com>
2021-07-28 13:09:04 -0300

    refactor blank lines

--- app/src/main/java/com/odougle/simplelogin/MainActivity.kt
+++ app/src/main/java/com/odougle/simplelogin/MainActivity.kt
@@ -2,15 +2,11 @@ package com.odougle.simplelogin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.odougle.simplelogin.ui.profile.ProfileFragment
-import com.odougle.simplelogin.ui.start.StartFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-
     }
 
 }
