28d205b3273806cb03b68f192f246ad0f02c6836
douguizilla <douglasgommez@gmail.com>
2021-07-31 20:02:38 -0300

    start to create a extension to set animations programmatically

---
+++ app/src/main/java/com/odougle/simplelogin/extensions/NavExtensions.kt
@@ -0,0 +1,11 @@
+package com.odougle.simplelogin.extensions
+
+import androidx.navigation.NavOptions
+import com.odougle.simplelogin.R
+
+private val navOptions = NavOptions.Builder()
+    .setEnterAnim(R.anim.slide_in_right)
+    .setExitAnim(R.anim.slide_out_left)
+    .setPopEnterAnim(R.anim.slide_in_left)
+    .setPopExitAnim(R.anim.slide_out_right)
+    .build()
