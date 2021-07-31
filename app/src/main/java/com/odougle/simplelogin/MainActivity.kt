942b3519f7182e54dec22e6528d6b2ee0d315998
douguizilla <douglasgommez@gmail.com>
2021-07-31 19:12:10 -0300

    set navController and appBarCOnfiguration

--- app/src/main/java/com/odougle/simplelogin/MainActivity.kt
+++ app/src/main/java/com/odougle/simplelogin/MainActivity.kt
@@ -2,11 +2,23 @@ package com.odougle.simplelogin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var navController: NavController
+    private lateinit var appBarConfiguration: AppBarConfiguration
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setSupportActionBar(myToolBar)
+
+        navController = findNavController(R.id.navHostFragment)
+        appBarConfiguration = AppBarConfiguration(navController.graph)
     }
 
 }
