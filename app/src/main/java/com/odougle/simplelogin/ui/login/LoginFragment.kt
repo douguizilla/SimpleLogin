b8f23a9c72729f0aaa465a8f4478562d7b767de8
douguizilla <douglasgommez@gmail.com>
2021-07-31 20:48:53 -0300

    create login layout

---
+++ app/src/main/java/com/odougle/simplelogin/ui/login/LoginFragment.kt
@@ -0,0 +1,28 @@
+package com.odougle.simplelogin.ui.login
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.odougle.simplelogin.R
+
+class LoginFragment : Fragment() {
+
+    private lateinit var viewModel: LoginViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.login_fragment, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(LoginViewModel::class.java)
+
+    }
+
+}
---
+++ app/src/main/java/com/odougle/simplelogin/ui/login/LoginViewModel.kt
@@ -0,0 +1,6 @@
+package com.odougle.simplelogin.ui.login
+
+import androidx.lifecycle.ViewModel
+
+class LoginViewModel : ViewModel() {
+}
