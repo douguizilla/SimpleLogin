528dee09e7d8da217e33defee0952e2bf99099a9
douguizilla <douglasgommez@gmail.com>
2021-08-01 15:09:49 -0300

    set onViewCreated and the first state to observe

--- app/src/main/java/com/odougle/simplelogin/ui/registration/profile/ProfileDataFragment.kt
+++ app/src/main/java/com/odougle/simplelogin/ui/registration/profile/ProfileDataFragment.kt
@@ -5,14 +5,32 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.Observer
 import com.odougle.simplelogin.R
+import com.odougle.simplelogin.ui.registration.RegistrationViewModel
 
 class ProfileDataFragment : Fragment() {
+
+    private val registrationViewModel: RegistrationViewModel by activityViewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_profile_data, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        registrationViewModel.registrationStateEvent.observe(viewLifecycleOwner, Observer { registrationState ->
+            when(registrationState){
+                is RegistrationViewModel.RegistrationState.CollectCredentials ->{
+
+                }
+            }
+
+        })
     }
 }
