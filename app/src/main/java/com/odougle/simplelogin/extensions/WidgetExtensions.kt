16b8c590bf69123414a6e70df6cb0e3e9ead1ddc
douguizilla <douglasgommez@gmail.com>
2021-08-01 14:05:43 -0300

    create fragments to registration

--- app/src/main/java/com/odougle/simplelogin/extensions/WidgetExtensions.kt
+++ app/src/main/java/com/odougle/simplelogin/extensions/WidgetExtensions.kt
@@ -1,7 +1,6 @@
 package com.odougle.simplelogin.extensions
 
 import com.google.android.material.textfield.TextInputLayout
-import kotlinx.android.synthetic.main.login_fragment.*
 
 fun TextInputLayout.dismissError(){
     this.error = null
---
+++ app/src/main/java/com/odougle/simplelogin/ui/registration/choosecredentials/ChooseCredentialsFragment.kt
@@ -0,0 +1,18 @@
+package com.odougle.simplelogin.ui.registration.choosecredentials
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.odougle.simplelogin.R
+
+class ChooseCredentialsFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_choose_credentials, container, false)
+    }
+}
