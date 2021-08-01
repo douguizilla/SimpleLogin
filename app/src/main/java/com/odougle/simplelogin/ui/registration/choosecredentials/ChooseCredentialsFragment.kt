535c7babf7bf0d83781bcab182bdcbb85d784b12
douguizilla <douglasgommez@gmail.com>
2021-08-01 15:46:47 -0300

    set ChooseCredentialsFragment.kt to receive name by navArgs and show it on screen

--- app/src/main/java/com/odougle/simplelogin/ui/registration/choosecredentials/ChooseCredentialsFragment.kt
+++ app/src/main/java/com/odougle/simplelogin/ui/registration/choosecredentials/ChooseCredentialsFragment.kt
@@ -5,14 +5,25 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.navArgs
 import com.odougle.simplelogin.R
+import kotlinx.android.synthetic.main.fragment_choose_credentials.*
 
 class ChooseCredentialsFragment : Fragment() {
+
+    val args: ChooseCredentialsFragmentArgs by navArgs()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_choose_credentials, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        //setando o nome recebido do fragment anterior
+        textChooseCredentialsName.text = getString(R.string.choose_credentials_text_name, args)
     }
 }
