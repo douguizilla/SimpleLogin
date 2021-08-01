20adac13707ec3ff8080a717dfc29a3598b13ff7
douguizilla <douglasgommez@gmail.com>
2021-07-31 21:34:22 -0300

    set error cases

--- app/src/main/java/com/odougle/simplelogin/ui/login/LoginFragment.kt
+++ app/src/main/java/com/odougle/simplelogin/ui/login/LoginFragment.kt
@@ -6,7 +6,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.Observer
+import com.google.android.material.textfield.TextInputLayout
 import com.odougle.simplelogin.R
+import kotlinx.android.synthetic.main.login_fragment.*
 
 class LoginFragment : Fragment() {
 
@@ -25,4 +28,30 @@ class LoginFragment : Fragment() {
 
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        viewModel.authenticationsStateEvent.observe(viewLifecycleOwner, Observer { authenticationState ->
+            when(authenticationState){
+                is LoginViewModel.AuthenticationState.InvalidAuthentication -> {
+                    val validationFields: Map<String, TextInputLayout> = initValidationFields()
+
+                    authenticationState.fields.forEach{ fieldError ->
+                        validationFields[fieldError.first]?.error = getString(fieldError.second)
+                    }
+                }
+            }
+        })
+
+        buttonLoginSignIn.setOnClickListener {
+            val username = inputLoginUsername.text.toString()
+            val password = inputLoginPassword.text.toString()
+            viewModel.authentication(username, password)
+        }
+    }
+
+    private fun initValidationFields() = mapOf(
+        LoginViewModel.INPUT_USERNAME.first to inputLayoutLoginUsername,
+        LoginViewModel.INPUT_PASSWORD.first to inputLayoutLoginPassword
+    )
 }
--- app/src/main/java/com/odougle/simplelogin/ui/login/LoginViewModel.kt
+++ app/src/main/java/com/odougle/simplelogin/ui/login/LoginViewModel.kt
@@ -1,6 +1,39 @@
 package com.odougle.simplelogin.ui.login
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.odougle.simplelogin.R
 
 class LoginViewModel : ViewModel() {
+    sealed class AuthenticationState{
+        class InvalidAuthentication(val fields: List<Pair<String,Int>>): AuthenticationState()
+    }
+
+    val authenticationsStateEvent = MutableLiveData<AuthenticationState>()
+
+    fun authentication(username: String, password: String){
+
+    }
+
+    private fun isValidForm(username: String, password: String): Boolean {
+        val invalidFilds = arrayListOf<Pair<String,Int>>()
+        if(username.isEmpty()){
+            invalidFilds.add(INPUT_USERNAME)
+        }
+
+        if(password.isEmpty()){
+            invalidFilds.add(INPUT_PASSWORD)
+        }
+
+        if(invalidFilds.isNotEmpty()){
+            authenticationsStateEvent.value = AuthenticationState.InvalidAuthentication(invalidFilds)
+            return false
+        }
+        return true
+    }
+
+    companion object{
+        val INPUT_USERNAME = "INPUT_USERNAME" to R.string.login_input_layout_error_invalid_username
+        val INPUT_PASSWORD = "INPUT_PASSWORD" to R.string.login_input_layout_error_invalid_password
+    }
 }
