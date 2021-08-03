978dc22bcfef7be1a698d4ee31af690729a302d3
douguizilla <douglasgommez@gmail.com>
2021-08-03 18:39:34 -0300

    fix dependencies, start to create database

--- app/src/main/java/com/odougle/simplelogin/data/db/UserEntity.kt
+++ app/src/main/java/com/odougle/simplelogin/data/db/UserEntity.kt
@@ -1,8 +1,13 @@
 package com.odougle.simplelogin.data.db
 
 import androidx.room.Entity
+import androidx.room.PrimaryKey
 
 @Entity(tableName = "user")
-class UserEntity:  {
-
-}
+data class UserEntity(
+    @PrimaryKey val id: Long = 0,
+    val name: String,
+    val bio: String,
+    val username: String,
+    val password: String
+)
