eb7e432adeb48bdbe8cc9c5e05900bf4d6397cdd
douguizilla <douglasgommez@gmail.com>
2021-08-03 18:48:02 -0300

    add annotation

--- app/src/main/java/com/odougle/simplelogin/data/db/AppDatabase.kt
+++ app/src/main/java/com/odougle/simplelogin/data/db/AppDatabase.kt
@@ -1,9 +1,11 @@
 package com.odougle.simplelogin.data.db
 
 import android.content.Context
+import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 
+@Database(entities = [UserEntity::class], version = 1)
 abstract class AppDatabase: RoomDatabase() {
     companion object{
         //Singleton prevents multiple instances of database opening at the
