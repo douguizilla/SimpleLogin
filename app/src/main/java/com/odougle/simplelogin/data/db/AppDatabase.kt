aaceb12f8fc3df3106ed853d0c3ba20b93ed2929
douguizilla <douglasgommez@gmail.com>
2021-08-03 21:15:13 -0300

    finish Dao and create abstract fun to call it

--- app/src/main/java/com/odougle/simplelogin/data/db/AppDatabase.kt
+++ app/src/main/java/com/odougle/simplelogin/data/db/AppDatabase.kt
@@ -4,9 +4,13 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import com.odougle.simplelogin.data.db.dao.UserDao
 
 @Database(entities = [UserEntity::class], version = 1)
 abstract class AppDatabase: RoomDatabase() {
+
+    abstract fun userDao(): UserDao
+
     companion object{
         //Singleton prevents multiple instances of database opening at the
         //same time
--- app/src/main/java/com/odougle/simplelogin/data/db/dao/UserDao.kt
+++ app/src/main/java/com/odougle/simplelogin/data/db/dao/UserDao.kt
@@ -3,6 +3,7 @@ package com.odougle.simplelogin.data.db.dao
 import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
+import androidx.room.Query
 import com.odougle.simplelogin.data.db.UserEntity
 
 @Dao
@@ -10,4 +11,10 @@ interface UserDao {
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     fun save(user: UserEntity) //min 20
+
+    @Query("SELECT * FROM user WHERE id = :id")
+    fun getUser(id: Long): UserEntity
+
+    @Query("SELECT id FROM user WHERE username = :username and password = :password")
+    fun login(username: String, password: String): Long
 }
