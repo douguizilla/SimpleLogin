6a52e3ece76ec06bf3fd549acd565358dd9e2b85
douguizilla <douglasgommez@gmail.com>
2021-08-03 21:36:22 -0300

    created extended function to convert RegistrationViewParams to UserEntity

--- app/src/main/java/com/odougle/simplelogin/data/db/UserEntity.kt
+++ app/src/main/java/com/odougle/simplelogin/data/db/UserEntity.kt
@@ -2,6 +2,7 @@ package com.odougle.simplelogin.data.db
 
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import com.odougle.simplelogin.ui.registration.RegistrationViewParams
 
 @Entity(tableName = "user")
 data class UserEntity(
@@ -11,3 +12,14 @@ data class UserEntity(
     val username: String,
     val password: String
 )
+
+fun RegistrationViewParams.toUserEntity(): UserEntity{
+    return with(this){
+        UserEntity(
+            name = this.name,
+            bio = this.bio,
+            username = this.username,
+            password = this.password
+        )
+    }
+}
